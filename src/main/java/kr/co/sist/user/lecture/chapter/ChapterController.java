106f1063f27cdf8fac05b14b6ec3441ad71aeb59
csjSIST <csj9630@gmail.com>
2026-02-02 17:49:08 +0900

    오후 5:48 2026-02-02 왜 db 연결이 안되는걸까

--- src/main/java/kr/co/sist/user/lecture/chapter/ChapterController.java
+++ src/main/java/kr/co/sist/user/lecture/chapter/ChapterController.java
@@ -1,15 +1,19 @@
 package kr.co.sist.user.lecture.chapter;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/chapter")
-@Controller
+@RequestMapping("/lecture/chapter")
+//@Controller
+@RestController
 public class ChapterController {
-	@Autowired(required = false)
+	@Autowired
 	private ChapterService cs;
 
 	@GetMapping("/")
@@ -20,4 +24,14 @@ public class ChapterController {
 		return "youtubeTest/youtubeTest";
 	}// method
 
+	@GetMapping("/viewList")
+	public String viewChapter(Model model) {
+
+		List<ChapterDomain> list = cs.searchAllChapter();
+		model.addAttribute("chapterList",list);
+
+		return list.toString();
+	}// method
+	
+	
 }// class
