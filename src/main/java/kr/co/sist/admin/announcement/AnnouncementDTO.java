b2773c80c49c6ed1f8cd4e5477caac593b6063b8
ljw0232 <ljw0232@gamil.com>
2026-02-03 16:32:50 +0900

    2026-02-03 오후 4:32 이지원 통합 첫 commit

---
+++ src/main/java/kr/co/sist/admin/announcement/AnnouncementDTO.java
@@ -0,0 +1,21 @@
+package kr.co.sist.admin.announcement;
+
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Alias("announcementDTO")
+@Getter
+@Setter
+@ToString
+public class AnnouncementDTO {
+ 
+	private int ann_id, views;
+	private String title, content, regip, adm_id; 
+	private Date regdate;
+	
+}
