d9bbb9e0dbd7e559671b51adbad7b73ebaec8453
LeejeongwooKuma <tkdeod1234@naver.com>
2026-02-03 16:21:40 +0900

    2026-02-03 16:21 이정우 첫 병합 commit

---
+++ src/main/java/kr/co/sist/instructor/lecture/test/InstTestMapper.java
@@ -0,0 +1,16 @@
+package kr.co.sist.instructor.lecture.test;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.exceptions.PersistenceException;
+
+@Mapper
+public interface InstTestMapper {
+	//Test테이블에 값 삽입
+	public int insertTest(InstTestDTO iDTO) throws PersistenceException;
+	//Test_Questtion 테이블에 값 삽입
+	public int insertTestQuestion(InstTestDTO iDTO) throws PersistenceException;
+	//모든 시험 문제 조회
+	public List<InstTestDomain> selectAllInstTest(InstTestViewDTO itvDTO) throws PersistenceException;
+}
