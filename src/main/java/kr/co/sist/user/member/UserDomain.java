0e9189dd6e2acda5d2743215f64bf7c19abd6df5
tlstmdejr <seung180@semyung.ac.kr>
2026-02-10 16:29:06 +0900

    오후 4:29 2026-02-10

--- src/main/java/kr/co/sist/user/member/UserDomain.java
+++ src/main/java/kr/co/sist/user/member/UserDomain.java
@@ -2,8 +2,6 @@ package kr.co.sist.user.member;
 
 import java.sql.Date;
 
-import org.apache.ibatis.type.Alias;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,99 +9,10 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@Alias("UserDomain")
 public class UserDomain {
 
     private String id, password, email, intro, name, img, phone, regIp;
     private Date birth, regDate;
-    private char activation;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getIntro() {
-        return intro;
-    }
-
-    public void setIntro(String intro) {
-        this.intro = intro;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getRegIp() {
-        return regIp;
-    }
-
-    public void setRegIp(String regIp) {
-        this.regIp = regIp;
-    }
-
-    public Date getBirth() {
-        return birth;
-    }
-
-    public void setBirth(Date birth) {
-        this.birth = birth;
-    }
-
-    public Date getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(Date regDate) {
-        this.regDate = regDate;
-    }
-
-    public char getActivation() {
-        return activation;
-    }
-
-    public void setActivation(char activation) {
-        this.activation = activation;
-    }
+    private int activation;
 
 }
