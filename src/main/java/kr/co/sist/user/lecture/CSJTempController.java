bc3e0c1aedb25135c0bcf45a5b6b1f9aea555ac9
csj1972 <csj9630@gmail.com~>
2026-02-12 02:14:40 +0900

    오전 2:14 2026-02-12

--- src/main/java/kr/co/sist/user/lecture/CSJTempController.java
+++ src/main/java/kr/co/sist/user/lecture/CSJTempController.java
@@ -3,6 +3,9 @@ package kr.co.sist.user.lecture;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * 개발용 임시 컨트롤러. 배포 전 제거할 것.
+ */
 @Controller
 public class CSJTempController {
 	
--- src/main/java/kr/co/sist/user/lecture/chapter/VideoDomain2.java
+++ src/main/java/kr/co/sist/user/lecture/chapter/VideoDomain2.java
@@ -22,6 +22,41 @@ public class VideoDomain2 {
 	private String title, videoUrl;
 	int num, progTime, videoLength, progress, state;
 	
+	
+	/** 초 시간을 '시:분:초'의 문자열로 변환.
+	 * @param second
+	 * @return
+	 */
+	public String convertHms(int second) {
+		int h = second / 3600;
+		int m = (second % 3600) / 60;
+		int s = second % 60;
+
+		if (h > 0) {
+			return String.format("%d:%02d:%02d", h, m, s);
+		} else {
+			return String.format("%d:%02d", m, s);
+		}
+	}//method
+	/**
+	 * 영상 시간 등을 초에서 시분초 문자열로 변환
+	 * 타임리프에서 progTimeStr로 호출 가능.
+	 * @return
+	 */
+	public String getProgTimeStr() {
+		return convertHms(this.progTime);
+	}//method
+	
+	/**
+	 * 영상 시간 등을 초에서 시분초 문자열로 변환
+	 * 타임리프에서 videoLengthStr로 호출 가능.
+	 * @return
+	 */
+	public String getVideoLengthStr() {
+		return convertHms(this.videoLength);
+	}//method
+
+
 	//List<String> videoList;
 	//Date lastDate;
 }
