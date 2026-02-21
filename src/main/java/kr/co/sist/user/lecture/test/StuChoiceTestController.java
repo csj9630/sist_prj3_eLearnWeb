6e7bd701786bdba474e81b24edea6bcb9344657b
jeongwoo <tkdeod1234@naver.com>
2026-02-21 20:55:39 +0900

    2026-02-21 이정우 작업 라스트

--- src/main/java/kr/co/sist/user/lecture/test/StuChoiceTestController.java
+++ src/main/java/kr/co/sist/user/lecture/test/StuChoiceTestController.java
@@ -1,5 +1,6 @@
 package kr.co.sist.user.lecture.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,20 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class StuChoiceTestController {
 
+	@Autowired
+	private StuTestService sts;
+	
 		// 시험 선택 Frm 띄워주기(시험 시작 or 지난 시험 및 해설)
 		@GetMapping("/choiceTestFrm")
-		public String instTestFrm(Model model, String lectId, String instId , HttpSession session) {
+		public String instTestFrm(Model model, String lectId, HttpSession session) {
+			
+			String testId = sts.searchTestId(lectId);
+			String userId = (String) session.getAttribute("userId");
+			
+			
+			model.addAttribute("testId", testId);
+			model.addAttribute("userId", userId);
+			model.addAttribute("lectId", lectId);
 			
 			return "user/lecture/test/userTestChoiceFrm";
 		}// instTestFrm
