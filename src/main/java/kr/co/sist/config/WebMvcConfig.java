e3a2f4d6086f4f6038f1a5dc2e14c42d2277a8ce
csj1972 <csj9630@gmail.com~>
2026-02-25 17:04:12 +0900

    최종 커밋

--- src/main/java/kr/co/sist/config/WebMvcConfig.java
+++ src/main/java/kr/co/sist/config/WebMvcConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//end
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
